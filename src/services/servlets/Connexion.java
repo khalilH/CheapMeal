d2cf4644557276138ef0b2e3312617ad6bccfdc9
Patrick TRAN <patrick.tran@stl.net>
2016-10-03 18:42:46 +0200

    first commit

---
+++ src/services/servlets/Connexion.java
@@ -0,0 +1,42 @@
+package services.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Connexion
+ */
+public class Connexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Connexion() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id=request.getParameter("id");
+		String mdp=request.getParameter("mdp");
+		response.setContentType("text/html");
+		response.getWriter().append("L'utilisateur  ").append(id+" et "+mdp);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
