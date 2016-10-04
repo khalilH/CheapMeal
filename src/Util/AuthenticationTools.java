daff451b711ead8e964ed41fde61061c49a29df5
tranpatrick <patra94@hotmail.fr>
2016-10-04 22:16:39 +0200

    Ajout de la fonction d'inscription et quelques fonctions outils

---
+++ src/Util/AuthenticationTools.java
@@ -0,0 +1,53 @@
+package Util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AuthenticationTools {
+
+	public static boolean loginLibre(String login) throws SQLException{
+		boolean res = true;
+		Connection c;
+		c = DBStatic.getSQLConnection();
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery("select u from UTILISATEURS where u.login='"+login+"';");
+		if(rs.next())
+			res = false;
+		rs.close();
+		stmt.close();
+		c.close();
+		return res;
+	}
+	
+	public static boolean emailLibre(String email) throws SQLException{
+		boolean res = true;
+		Connection c;
+		c = DBStatic.getSQLConnection();
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery("select u from UTILISATEURS where u.mail='"+email+"';");
+		if(rs.next())
+			res = false;
+		rs.close();
+		stmt.close();
+		c.close();
+		return res;
+	}
+	
+	public static int getIdAvecLogin(String login) throws SQLException{
+		int id = 0;
+		Connection c;
+		c = DBStatic.getSQLConnection();
+		Statement stmt = c.createStatement();
+		ResultSet cur = stmt.executeQuery("select id from UTILISATEURS where login='"+login+"';");
+		if(cur.next())
+			id = cur.getInt("id");
+		cur.close();
+		stmt.close();
+		c.close();
+		return id;
+	}
+	
+	
+}
--- src/services/fonctions/UtilisateurFonctions.java
+++ src/services/fonctions/UtilisateurFonctions.java
@@ -1,28 +1,51 @@
 package services.fonctions;
 
-import org.json.JSONObject;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Util.AuthenticationTools;
+import Util.DBStatic;
+import exceptions.IDException;
+import exceptions.InformationUtilisateurException;
 
 public class UtilisateurFonctions {
 
-	public void inscription(String login, String mdp, String prenom, String nom, String email) throws Exception{
+	public void inscription(String login, String mdp, String prenom, String nom, String email) throws InformationUtilisateurException, IDException, SQLException{
 
 		/* Verification des parametres */
 		if(login == null || mdp == null || prenom == null || nom == null || email == null)
-			throw new Exception();
+			throw new InformationUtilisateurException("Informations non complètes");
 
 		if(login == "" || mdp == "" || prenom == "" || nom == "" || email == "")
-			throw new Exception();
+			throw new InformationUtilisateurException("Nom d'utilisateur, mot de passe ou adresse mail invalide");
 
 		if(mdp.length() < 6)
-			throw new Exception();
+			throw new InformationUtilisateurException("Mot de passe trop court");
 		
-		//Ici verifier si le login est available
-		
-		//Ici verifier si l'email est available
-		
+		if(!AuthenticationTools.loginLibre(login))
+			throw new InformationUtilisateurException("Ce nom d'utilisateur n'est pas libre");
+
+		if(!AuthenticationTools.emailLibre(email))
+			throw new InformationUtilisateurException("Cette adresse email est déjà utilisée");
+
 		/* Creation de l'utilisateur dans la base SQL */
+		ajoutUtilisateurBD(login, mdp, nom, prenom, email);
+		int id = AuthenticationTools.getIdAvecLogin(login);
+		if(id == 0)
+			throw new IDException("Utilisateur inexistant");
 		
+		//TODO Créer le profile ICI 
+		//ProfileFonctions.creerProfile(id);
 		
+	}
+	
+	private void ajoutUtilisateurBD(String login, String mdp, String nom, String prenom, String email) throws SQLException{
+		Connection c = DBStatic.getSQLConnection();
+		Statement stmt = c.createStatement();
+		stmt.executeQuery("insert into `UTILISATEURS` (login, mdp, prenom, nom, mail) values ('"+login+"','"+mdp+"','"+nom+"','"+prenom+"','"+email+"');");
+		stmt.close();
+		c.close();
 	}
 
 }
