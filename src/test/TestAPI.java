ebe317a8e134dfdba6de0562d6048994b4a3b77b
tranpatrick <patra94@hotmail.fr>
2016-11-03 15:46:51 +0100

    Ajout ExternalAPI.java

--- src/test/TestAPI.java
+++ src/test/TestAPI.java
@@ -1,31 +1,25 @@
 package test;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.ArrayList;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
+import org.json.JSONException;
+
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import util.ExternalAPI;
 
 public class TestAPI {
 
 	public static void main(String[] args) {
 		try {
-			HttpResponse<String> response = Unirest.get("https://datagram-products-v1.p.mashape.com/chains/")
-					.header("X-Mashape-Key", "KReKhv4jhXmshDBVbYlbtdiYGRJdp1ZOAKtjsnfyiGaxZnxQAJ")
-					.header("Accept", "application/json")
-					.asString();
-			
-			JSONArray array = new JSONArray(response.getBody());
-			System.out.println(array.toString(1));
-			JSONObject json = new JSONObject().put("results", array);
-			System.out.println(json.toString(1));
+			ArrayList<Double> prix = ExternalAPI.searchPrices("3451790941610");
+			System.out.println("TAILLE = "+prix.size());
+			for (Double d : prix) {
+				System.out.println(d.toString());
+			}
 		} catch (UnirestException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
