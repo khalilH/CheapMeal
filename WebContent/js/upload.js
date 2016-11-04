97bf6c248972d881de325a4c9f4384c8c1181ebc
VictorADS <victor.ads75@gmail.com>
2016-11-04 23:47:14 +0100

    working on bootstrap

---
+++ WebContent/js/upload.js
@@ -0,0 +1,22 @@
+$(function(){
+	
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#previewImage').attr('src', e.target.result);
+            console.log($('#previewImage'));
+            }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$("#upload").change(function(){
+	console.log($(this).val);
+    readURL(this);
+});
+
+});
