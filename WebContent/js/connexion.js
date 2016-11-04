babcbe8322b9932736db2d07950419ae87354235
VictorADS <victor.ads75@gmail.com>
2016-11-04 17:57:34 +0100

    Modal box for forgot

--- WebContent/js/connexion.js
+++ WebContent/js/connexion.js
@@ -28,6 +28,7 @@ $(function() {
 			}
 		},
 		submitHandler : function(form) {
+			console.log("toyze");
 			requeteAJAX(form.login.value,form.password.value);
 		}
 	});
@@ -66,4 +67,37 @@ $(function() {
 							"</div>");
 	}
 	
+	$("#forgot").validate({
+		rules: {
+			forgotPassword :{required : true}
+		},
+		messages:{
+			forgotPassword:"<p> Vous devez saisir un mail</p>"
+		},
+		submitHandler : function(form){
+			console.log("valid");
+		}
+	});
+	$("#send").on('click',function(){
+		$("#forgot").validate();
+		console.log("validee");
+
+	});
+	$('#forgotModal').on('shown.bs.modal',function(){
+	    document.activeElement.blur()
+		$('#forgotPassword').focus();
+		$('.tooltip').remove();
+	})
+	 function alignModal(){
+	        var modalDialog = $(this).find(".modal-dialog");
+	        // Applying the top margin on modal dialog to align it vertically center
+	        modalDialog.css("margin-top", Math.max(0, ($(window).height() - modalDialog.height()) / 4));
+	    }
+	    // Align modal when it is displayed
+	    $(".modal").on("shown.bs.modal", alignModal);
+	    
+	    // Align modal when user resize the window
+	    $(window).on("resize", function(){
+	        $(".modal:visible").each(alignModal);
+	    });   
 });
