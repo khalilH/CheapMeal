2d81fc382696f5bde3b173223764d637d8053513
VictorADS <victor.ads75@gmail.com>
2016-11-05 18:05:20 +0100

    Upload image now working

--- WebContent/js/upload.js
+++ WebContent/js/upload.js
@@ -1,22 +1,38 @@
-$(function(){
+$(function() {
+
+	function readURL(input) {
+
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				$('#previewImage').attr('src', e.target.result);
+			}
+
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	$("#uploadInput").change(function() {
+		readURL(this);
+	});
+
+	$("#uploadForm").on('submit',function(event){
+		event.preventDefault();
+		var data = new FormData();
+		console.log(this.uploadInput.files[0]);
+		data.append('file-0', this.uploadInput.files[0]);
+		jQuery.ajax({
+	    url: 'upload',
+	    data: data,
+	    cache: false,
+	    contentType: false,
+	    processData: false,
+	    type: 'POST',
+	    success: function(data){
+	        console.log(data);
+	    }
+		});
+	});
 	
-function readURL(input) {
-
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#previewImage').attr('src', e.target.result);
-            console.log($('#previewImage'));
-            }
-
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-
-$("#uploadInput").change(function(){
-	console.log($(this).val);
-    readURL(this);
-});
-
 });
--- src/services/servlets/UploadImage.java
+++ src/services/servlets/UploadImage.java
@@ -1,17 +1,23 @@
 package services.servlets;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import services.ProfilServices;
+@MultipartConfig
 
 public class UploadImage extends  HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,23 +28,25 @@ public class UploadImage extends  HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		/* Lecture des parametres */
-		String cle = request.getParameter("cle");
-		String login = request.getParameter("login");
-		
-				
-		try {
-			/* Traitement des services */
-			JSONObject res = ProfilServices.afficherBio(cle, login);
+        String path=  "";
+		  InputStream inputStream = null; // input stream of the upload file
+	        // obtains the upload file part in this multipart request
+	        Part filePart = request.getPart("file-0");
+	        if (filePart != null) {
+	            // prints out some information for debugging
+	            System.out.println(filePart.getName());
+	            System.out.println(filePart.getSize());
+	            System.out.println(filePart.getContentType());
+	             
+	            // obtains input stream of the upload file
+	            inputStream = filePart.getInputStream();
+	            BufferedImage image = ImageIO.read(inputStream);
+	            File f = new File("/home/ladi/images/test.png");
+	           ImageIO.write(image, "png", new File("/home/ladi/images/test.png"));
+	        }
+	        PrintWriter writer = response.getWriter();
+			response.setContentType("plain/text");
+			writer.println(path);
 			
-			/* Ecriture de la reponse */
-			PrintWriter writer = response.getWriter();
-			response.setContentType("application/json");
-			writer.println(res.toString());
-			
-		} catch (JSONException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
