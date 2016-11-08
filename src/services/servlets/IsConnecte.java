a7b8cb977ec41e8c677cb8f4987c007fd8576552
khalilH <halifa.ladislas@gmail.com>
2016-11-08 03:11:49 +0100

    fix IsConnecte.java file

--- src/services/servlets/IsConnecte.java
+++ src/services/servlets/IsConnecte.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,7 +13,9 @@ import org.json.JSONObject;
 
 import services.ConnexionServices;
 
-public class isConnecte {
+public class IsConnecte extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
