5e8c48fe576b005de5b4917d4e8f5b13bc594cba
VictorADS <victor.ads75@gmail.com>
2016-11-09 13:29:04 +0100

    Refactor navbar style and js

---
+++ WebContent/js/navbar.js
@@ -0,0 +1,47 @@
+$(function(){
+$("#deconnexion").on('click',function(){
+		// TODO Delete cookie + verifier sil existe (erreur)
+		var key = getCookie(C_NAME_KEY);
+		console.log("DeConnexion de "+key);
+		
+		$.ajax({
+			url : 'user/deconnexion',
+			type : 'POST',
+			data : 'cle='+key,
+			contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+			dataType : 'json',
+			success : function(rep) {
+				console.log(JSON.stringify(rep));
+				var jsonrep = JSON.stringify(rep)
+				var json = JSON.parse(jsonrep);
+				destroy_cookie();
+				window.location.href="accueil.html";
+				return;
+			},
+			error : function(resultat, statut, erreur) {
+				console.log("Bug");
+				console.log(resultat);
+				alert("dawg");
+			}
+		});
+		
+	});
+	
+
+	
+	$("#connexion").on('click',function(){
+		window.location.href="connexion.html";
+		return;
+	});
+	$("#signup").on('click',function(){
+			window.location.href="inscription.html";
+			return;
+	});
+		
+	
+	$('.recette').on('click',function(){
+		console.log("J'ai clique sur une recette ",this.id);
+		//TODO Afficher la page de la recette
+	});
+	
+});
