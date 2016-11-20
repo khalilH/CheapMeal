b3efb5a44d91d56657e724b1f59841446eb7dcf6
VictorADS <victor.ads75@gmail.com>
2016-11-20 14:14:08 +0100

    Routing compris manque des informations a transmettre entre controlleur

--- WebContent/js/test.js
+++ WebContent/js/test.js
@@ -3,54 +3,145 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(function($routeProvider){
 	$routeProvider
-	.when('/',{templateUrl: 'Test2.html'})
-	.when('/comments',{templateUrl : 'Test3.html'})
+	.when('/',{templateUrl: 'Test2.html',controller:'PostsController'})
+	.when('/comments/:id',{templateUrl : 'Test3.html',controller :'commentController'})
 	.otherwise({redirectTo : '/'})
+});
+
+app.controller('PostsController',function ($scope){
+	console.log($scope);
+	$scope.companies = [
+		  {
+		    "id": 0,
+		    "company": "Animalia",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Daisy Everett"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Benton Mayo"
+		      },
+		      {
+		        "id": 2,
+		        "name": "May Moses"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 1,
+		    "company": "Nurplex",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Carrillo Mcbride"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Eula Mcclain"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Valdez Wall"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 2,
+		    "company": "Surelogic",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Mason Mays"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Concetta Burris"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Flossie Hall"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 3,
+		    "company": "Valreda",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Sweeney Mathis"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Sharlene Floyd"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Ellis Christensen"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 4,
+		    "company": "Dancity",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Jerri Nielsen"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Kerry Rollins"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Mcdaniel Fulton"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 5,
+		    "company": "Globoil",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Crane Chang"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Brock Ayala"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Rocha Hayden"
+		      }
+		    ]
+		  },
+		  {
+		    "id": 6,
+		    "company": "Rubadub",
+		    "comments": [
+		      {
+		        "id": 0,
+		        "name": "Chan Wilder"
+		      },
+		      {
+		        "id": 1,
+		        "name": "Nunez Figueroa"
+		      },
+		      {
+		        "id": 2,
+		        "name": "Battle Thomas"
+		      }
+		    ]
+		  }
+	];
+
 });
 
 app.controller('commentController',function ($scope){
 	console.log($scope);
-	$scope.comments = [
-	{
-		"firstname": "Cannon",
-		"city": "Irwin"
-	},
-	{
-		"firstname": "Joy",
-		"city": "Trucksville"
-	},
-	{
-		"firstname": "Gloria",
-		"city": "Graball"
-	},
-	{
-		"firstname": "Rivera",
-		"city": "Craig"
-	},
-	{
-		"firstname": "Moreno",
-		"city": "Lewis"
-	},
-	{
-		"firstname": "Landry",
-		"city": "Caron"
-	},
-	{
-		"firstname": "Duncan",
-		"city": "Southmont"
-	},
-	{
-		"firstname": "Farley",
-		"city": "Alfarata"
-	},
-	{
-		"firstname": "Finch",
-		"city": "Kiskimere"
-	},
-	{
-		"firstname": "Leanna",
-		"city": "Enoree"
-	}
-	];
 
 });
